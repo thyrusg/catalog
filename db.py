4a22f64d4c1919bceb191401e86371461f0b55e3
thyrusg <me@thyrusgorges.com>
2016-01-01 22:20:16 -0500

    Added db.py. This will handle all of our DB stuff. We will use SQLite3 for all DB stuff.

---
+++ db.py
@@ -0,0 +1,7 @@
+# Handle all of our Database stuff
+
+# Things we will handle
+# Making sure the database exists. If it doesn't create it. 
+# Searching through the database to find the files we want
+# Adding items to the database when we index a directory
+# Removing items from the database if they no longer exist. Not to sure on how I'd like to handle this one.
