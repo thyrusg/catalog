23b75b32026e25acd5da8c7e7c8ff15108c72737
thyrusg <me@thyrusgorges.com>
2016-01-02 21:42:11 -0500

    Added handling for each of the arguments. Problem is that we always need to supply two arguments when sometimes we only need one argument. For example, the index argument just needs the directory you want to index. It doesn't need a file name at all.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import argparse
 parser = argparse.ArgumentParser()
 
 # Argument for the filename the user can specify
-parser.add_argument("name", help="Name of the document", type=str)
+parser.add_argument("name", help="Name of the document", type=str, default=None)
 
 # Argument for indexing option
 parser.add_argument("-i", "--index", help="Search the current directory for specified file types and add to DB")
@@ -14,3 +14,15 @@ parser.add_argument("-s", "--search", help="Search DB for the name and return so
 parser.add_argument("-o", "--open", help="Open up the document in the OS's default application")
 
 args = parser.parse_args()
+
+# Execute the index option
+if args.index:
+    print "Indexing the directory"
+
+# Execute the search option
+if args.search:
+    print "Searching for the file"
+
+# Execute the open option
+if args.open:
+    print "Opening the file"
